57be29a47728286b40f9016500d607c0ea5ac872
HiorCraft <200645597+HiorCraft@users.noreply.github.com>
2025-09-29 20:30:58 +0200

    Add Feat: Main

---
+++ src/main/kotlin/de/hiorcraft/nex/nexmoderation/Nexmoderation.kt
@@ -0,0 +1,14 @@
+package de.hiorcraft.nex.nexmoderation
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Nexmoderation : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
