af7cb7d69564ad5c15fd2bc97d50b0995feec06c
HiorCraft <200645597+HiorCraft@users.noreply.github.com>
2025-09-29 20:53:02 +0200

    Add Feat: Main code

--- src/main/kotlin/de/hiorcraft/nex/nexmoderation/NexModeration.kt
+++ src/main/kotlin/de/hiorcraft/nex/nexmoderation/NexModeration.kt
@@ -4,11 +4,46 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class NexModeration : JavaPlugin() {
 
+    companion object{
+        lateinit var instance: NexModeration
+            private set
+    }
+
+    lateinit var dataSourece: DataSource
+    lateinit var punishmentManager: PunishmentManager
+
+
     override fun onEnable() {
-        // Plugin startup logic
+        logger.info("NexModeration Starting...")
+        instance = this
+        saveDefaultConfig()
+
+        setupDatabase()
+        punishmentManager = PunishmentManager(dataSourece)
+
+        // Comand registration
+        BanCommand(punishmentManager).register()
+        MuteCommand(punishmentManager).register()
+        WarnCommand(punishmentManager).register()
+
+
+        logger.info("NexModeration enabled")
+    }
+
+    private fun setupDatabase() {
+        val cfg = HikariConfig().apply {
+            jdcUrl = "jdbc:mysql://${getConfig().getString("database.host")}:${getConfig().getInt("database.port")}/${getConfig().getString("database.name")}"
+            username = getConfig().getString("database.user")
+            password = getConfig().getString("database.password")
+
+           }
+        dataSourece = HikariDataSource(cfg)
+        }
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        logger.info("NexModeration disabled")
+
+        logger.info("Bye <3")
     }
 }
